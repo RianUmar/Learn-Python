5f4e466782d0fe59075d048d98f78405d9fabdfd
RianUmar <adriansyah.umar07@gmail.com>
2025-08-01 00:26:57 +0800

    belajar lagi men

--- input.py
+++ input.py
@@ -1,3 +1,8 @@
 data = bool(int(input("Masukkan : ")))
 
 print("Data = ", data, " Dengan type = ", type(data))
+
+nama = input("Masukkan nama Anda : ")
+umur = int(input("Masukkan umur Anda : "))
+
+print("Nama anda :",nama," Umur anda :", umur)
