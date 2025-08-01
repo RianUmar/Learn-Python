19078ef3241e0364d121726a7cae2e1727ed767c
RianUmar <adriansyah.umar07@gmail.com>
2025-08-01 17:42:02 +0800

    Still belajar

---
+++ konversi_suhu_temperatur.py
@@ -0,0 +1,21 @@
+# Latihan konversi suhu dan 
+
+print("==PROGRAM KONVERSI SUHU==")
+
+# Konversi celcius ke satuan lain
+
+suhu_celcius = float(input("Masukan suhu dalam celccius :"))
+print("Suhu dalam celcius adalah ", suhu_celcius)
+
+# reamur
+suhu_reamur = suhu_celcius * (4 / 5)
+print("Suhu dalam reamur adalah ", suhu_reamur)
+
+# fahrenheit
+suhu_fahrenheit = (9/5) * suhu_celcius + 32
+print("Suhu dalam fahrenheit adalah ", suhu_fahrenheit)
+
+# kelvin
+
+
+
