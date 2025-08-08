68e5a8235232cc53598597c55cae039c864ce56b
RianUmar <adriansyah.umar07@gmail.com>
2025-08-08 15:26:31 +0800

    still learning menn

---
+++ komparasi.py
@@ -0,0 +1,12 @@
+# KOMPARASI MENNN
+# Still Learning
+# Percaya Proses
+
+a = 10
+b = 5
+objek_komparasi = 8
+komparasi1 = a > objek_komparasi
+print (a, ">", objek_komparasi, "=", komparasi1)
+
+komparasi2 = b > objek_komparasi
+print (b, ">", objek_komparasi, "=", komparasi2)
